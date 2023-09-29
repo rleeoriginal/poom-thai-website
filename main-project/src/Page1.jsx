198bf7c6c9870bf1d25b1ed6cdb5ed0cef041b08
rleeoriginal <royleeoriginal@gmail.com>
2023-09-29 15:01:15 -0400

    menu: done

---
+++ main-project/src/Page1.jsx
@@ -0,0 +1,80 @@
+function Page1() {
+    return (<>
+        <div id="menu-container">
+            <div class="menu-title">SPECIALS</div>
+            <div class="dish">
+                <div class="dish-title">DINNER SPECIAL<span>17.95</span></div>
+                <div class="dish-desc">An individual portion of your favorite dish served with a salad, egg roll, and your choice of soup and rice.</div>
+                <div class="dish-desc">Menu item: #28-30, #33-35, #37-40; #58-67</div>
+                <div class="dish-desc">Soup: Wonton Soup, Tom Kha Kai, or Tom Yun Kai</div>
+                <div class="dish-desc">Rice: Shrimp fried rice, egg fried rice, white rice, or brown rice</div>
+            </div>
+            <div class="dish">
+                <div class="dish-title">B.B.Q COMBO<span>17.95</span></div>
+                <div class="dish-desc">Combo meal with BBQ Chicken, BBQ Pork Ribs, BBQ Beef, and shrimp fried rice.</div>
+            </div>
+            <div class="menu-title">APPETIZERS</div>
+            <div class="dish">
+                <div class="dish-title">1. EGG ROLL (4 ROLLS)<span>6.95</span></div>
+                <div class="dish-desc">Crispy vegetarian egg rolls served with
+                    sweet and sour & sauce.</div>
+            </div>
+            <div class="dish">
+                <div class="dish-title">2. FRIED HOT WONTON (8 WONTONS)<span>6.95</span></div>
+                <div class="dish-desc">Homemade crispy minced pork wontons
+                    shrimp & chicken.</div>
+            </div>
+            <div class="dish">
+                <div class="dish-title">3. CRISPY CALAMARI <span>11.95</span></div>
+                <div class="dish-desc">Calamari breaded with panko served with sweet & sour sauce.</div>
+            </div>
+            <div class="dish">
+                <div class="dish-title">4. SHRIMP TEMPURA <span>11.95</span></div>
+                <div class="dish-desc">Golden-fried jumbo shrimp and vegetables (broccoli, carrots, and bell pepper) served with sweet & sour sauce.</div>
+            </div>
+            <div class="dish">
+                <div class="dish-title">5. THE FOUR SEASON <span>11.95</span></div>
+                <div class="dish-desc">Sampler of egg rolls (2), shrimp tempura (2), crispy calamari, and fried hot wontons (3).</div>
+            </div>
+            <div class="dish">
+                <div class="dish-title">6. TOFU SATAY <span>10.95</span></div>
+                <div class="dish-desc">Satay tofu served with peanut sauce and cucumber salad.</div>
+            </div>
+            <div class="dish">
+                <div class="dish-title">7. CHICKEN OR BEEF SATAY <span>10.95</span></div>
+                <div class="dish-desc">Satay marinated chicken or beef served with peanut sauce and cucumber salad.</div>
+            </div>
+            <div class="dish">
+                <div class="dish-title">8. FISH CAKE (5 CAKES) <span>10.95</span></div>
+                <div class="dish-desc">Spicy fried fish cakes with sweet and sour
+                    sauce.</div>
+            </div>
+            <div class="dish">
+                <div class="dish-title">9. WING OF ANGELS (2 WINGS) <span>12.95</span></div>
+                <div class="dish-desc">Stuffed chicken wing with glassy noodles, shrimp & chicken.</div>
+            </div>
+            <div class="dish">
+                <div class="dish-title">10. DUMPLINGS <span>6.95</span></div>
+                <div class="dish-desc">Fried or steamed dumpling stuffed with mixed vegetables and served with brown sauce.</div>
+            </div>
+            <div class="dish">
+                <div class="dish-title">11. FRIED TOFU <span>6.95</span></div>
+                <div class="dish-desc">Deep-fried tofu squares served with sweet and sour sauce.</div>
+            </div>
+            <div class="dish"> 
+                <div class="dish-title">12. CREAM CHEESE PUFF <span>10.95</span></div>
+                <div class="dish-desc">Fried wonton skins stuffed with cheese, onion, and crabmeat.</div>
+            </div>
+            <div class="dish-info">
+                <div>
+                    PHONE IN FOR PICKUP OR ORDER DELIVERY ON POSTMATES, UBEREATS, OR GRUBHUB
+                </div>
+                <div>
+                    STEAMED RICE WHITE OR BROWN ONLY $2
+                </div>
+            </div>
+        </div>
+    </>)
+}
+
+export default Page1
---
+++ main-project/src/Page2.jsx
@@ -0,0 +1,78 @@
+function Page2() {
+    return (<>
+    <div id="menu-container">
+            <div className="menu-title">SOUPS</div>
+            <div className="dish">
+                <div className="dish-title">13. TOM KHA KAI <span>11.95</span></div>
+                <div className="dish-desc">Chicken and mushrooms in coconut milk with lemongrass, chili, and lime juice.
+</div>
+            </div>
+            <div className="dish">
+                <div className="dish-title">14. TOM YUM KOONG <span>12.95</span></div>
+                <div className="dish-desc">Shrimp and mushrooms in hot & sour soup with lemongrass, chili, and lime juice.</div>
+            </div>
+             <div className="dish">
+                <div className="dish-title">15. TOM YUM KAI <span>11.95</span></div>
+                <div className="dish-desc">Lemon glass chicken soup with exotic spices and lime juice.</div>
+             </div>
+             <div className="dish">
+                <div className="dish-title">16. PO-KAK (HOT & SOUR SEAFOOD SOUP) <span>20.95</span></div>
+                <div className="dish-desc">Assorted seafood in hot and sour soup with lemongrass, chili, and lime juice.</div>
+             </div>
+             <div className="dish">
+                <div className="dish-title">17. FISHERMAN'S SEAFOOD CHOWDER <span>20.95</span></div>
+                <div className="dish-desc">Coconut milk seafood soup with shrimp, squid, fish, mussels, and clams.</div>
+             </div>
+             <div className="dish">
+                <div className="dish-title">18. TOM YUM PAK (VEGETABLE LEMON GRASS SOUP) <span>11.95</span></div>
+                <div className="dish-desc">Assorted vegetables in hot and sour soup with lemongrass, chili, and lime juice.</div>
+             </div>
+             <div className="dish">
+                <div className="dish-title">19. WONTON SOUP <span>11.95</span></div>
+                <div className="dish-desc">Stuffed with vegetable, chicken, shrimp, and ground pork.</div>
+             </div>
+             <div className="dish">
+                <div className="dish-title">20. GLASS NOODLE AND TOFU SOUP <span>11.95</span></div>
+                <div className="dish-desc">Mixed vegetables with shrimp and chicken.</div>
+             </div>
+             <div className="menu-title">SALADS</div>
+             <div className="dish">
+                <div className="dish-title">21. HOUSE SALAD <span>11.95</span></div>
+                <div className="dish-desc">Chicken, mixed vegetable with peanut sauce.</div>
+             </div>
+             <div className="dish">
+                <div className="dish-title">22. SPICY YUM WOON SEN <span>11.95</span></div>
+                <div className="dish-desc">Spicy chicken salad with glass noodles, onions, romaine lettuce, carrots, chili, and lime juice.</div>
+             </div>
+             <div className="dish">
+                <div className="dish-title">23. BEEF SALAD <span>11.95</span></div>
+                <div className="dish-desc">Grilled beef salad with romaine lettuce, red onions, lemongrass, mint leaves, chili, and lime juice.</div>
+             </div>
+             <div className="dish">
+                <div className="dish-title">24. CHICKEN SALAD (LARB.) <span>11.95</span></div> 
+                <div className="dish-desc">Traditional Thai larb salad with minced chicken, mint leaves, chili, lime juice, and our homemade larb sauce.</div>
+             </div>
+             <div className="dish">
+                <div className="dish-title">25. PLAH KOONG <span>12.95</span></div>
+                <div className="dish-desc">Grilled shrimp with romaine lettuce, red onions, lemongrass, mint leaves, yum sauce, and lime juice.</div>
+             </div>
+             <div className="dish">
+                <div className="dish-title">26. YUM-PLA-MUK (SQUID SALAD) <span>11.95</span></div>
+                <div className="dish-desc">Cooked squids with red onions, lettuce, lemongrass, mint leaves, lime juice, and our house green sauce</div>
+             </div>
+             <div className="dish">
+                <div className="dish-title">26A. ENSALADA DE HUEVO (SPECIAL EGG SALAD) <span>12.95</span></div>
+                <div className="dish-desc">Pan-fried egg salad with cucumber, tomatoes, onions, carrots, chicken, and
+our house green sauce.</div>
+             </div>
+             
+
+
+
+        </div>
+    
+    
+    </>)
+}
+
+export default Page2
