36df2892fc7e2032b6b773300dcf337e5cc9c402
rleeoriginal <royleeoriginal@gmail.com>
2023-09-29 15:07:24 -0400

    menu: revision

--- js/script.js
+++ js/script.js
@@ -2,6 +2,10 @@ function navigateToMenu() {
     window.location.href = './menu.html';
 }
 
+function navigateToHome() {
+    window.location.href = './index.html'
+}
+
 function navigateToMap() {
     window.location.href = 'https://maps.google.com/maps?ll=34.004085,-118.471309&z=16&t=m&hl=en-US&gl=US&mapclient=embed&cid=11297211483168715120'
 }
